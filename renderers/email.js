310b57b1ed5f0482725c16d064d7c2b28abb1e09
Andrey Romanov <scorpion21.97@gmail.com>
2019-03-30 17:58:55 +0300

    Add support for images

--- renderers/email.js
+++ renderers/email.js
@@ -1,14 +1,32 @@
+function renderImage(item) {
+  if (!item.image) {
+    return "";
+  }
+
+  return `<mj-image src="${item.image}" alt="${item.title}" />\n`;
+}
+
+function renderTitle(item) {
+  return `<mj-text font-size="20px" line-height="30px" padding-bottom="0"><a href="${
+    item.url
+  }">${item.title}</a></mj-text>\n`;
+}
+
+function renderDescription(item) {
+  return `<mj-text line-height="24px">${item.description}</mj-text>\n`;
+}
+
+function renderDivider() {
+  return `\n<mj-divider border-width="1px" border-color="#e0e0e0"></mj-divider>\n\n`;
+}
+
 function renderForEmail(entry) {
   return entry.items
     .map(
-      item => `<mj-text font-size="20px" line-height="30px" padding-bottom="0"><a href="${
-        item.url
-      }">${item.title}</a></mj-text>
-<mj-text line-height="24px">${item.description}</mj-text>`
+      item =>
+        `${renderImage(item)}${renderTitle(item)}${renderDescription(item)}`
     )
-    .join(
-      `\n\n<mj-divider border-width="1px" border-color="#e0e0e0"></mj-divider>\n\n`
-    );
+    .join(renderDivider());
 }
 
 module.exports = { renderForEmail };
